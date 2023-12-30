68caa11e90ca3d7ede8b95be7f7f2368acf1dd8a
Abhiram Ghanta <abhiramg2021@gmail.com>
2023-12-30 15:50:58 -0500

    initial design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
-import { Button } from "@/components/ui/button"
-import { CardTitle, CardHeader, Card, CardContent } from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { CardTitle, CardHeader, Card, CardContent } from "@/components/ui/card";
 import { useState, useEffect, FormEvent } from "react";
 import { stringify } from "querystring";
-
 
 export interface Source {
   name: string;
@@ -15,43 +14,54 @@ export type Sources = Source[];
 
 export default function Home() {
   const [currentSource, setCurrentSource] = useState<string>("twitter");
+  const [modelType, setModelType] = useState<string>("gpt-4");
+
   const [sources, setSources] = useState<Sources>([]);
   const [summary, setSummary] = useState<string>("");
   const [newSource, setNewSource] = useState<string>("");
-  const [openAIKey, setOpenAIKey] = useState<string>(""); 
-  useEffect(() => {
-
-  }, [currentSource]);
+  const [openAIKey, setOpenAIKey] = useState<string>("");
+  useEffect(() => {}, [currentSource]);
 
   const deleteSource = (source: Source) => {
-    setSources(sources.filter(s => s !== source));
-  }
+    setSources(sources.filter((s) => s !== source));
+  };
 
   const addSource = async (event: FormEvent) => {
     event.preventDefault();
     if (currentSource === "twitter") {
       const data = await getTwitterSourceRSS(newSource);
       const image = data.image.url;
-      setSources([...sources, { name: newSource, image, sourceType: currentSource }]);
+      setSources([
+        ...sources,
+        { name: newSource, image, sourceType: currentSource },
+      ]);
     } else {
-      setSources([...sources, { name: newSource, image: "https://images.squarespace-cdn.com/content/v1/5c5554d316b64061c6f8a20d/1630949829757-WXNOUZ8R4QQCXMIY4YMG/What-Is-The-Reddit-Logo-Called.png", sourceType: currentSource }]); // Each source now has a name and an image
+      setSources([
+        ...sources,
+        {
+          name: newSource,
+          image:
+            "https://images.squarespace-cdn.com/content/v1/5c5554d316b64061c6f8a20d/1630949829757-WXNOUZ8R4QQCXMIY4YMG/What-Is-The-Reddit-Logo-Called.png",
+          sourceType: currentSource,
+        },
+      ]); // Each source now has a name and an image
       setNewSource("");
     }
-  }
+  };
 
   const getTwitterSourceRSS = async (sourceName: string) => {
     const url = `https://nitter.1d4.us/${sourceName}/rss`;
     const response = await fetch(`/api/getRSSFeed?url=${url}`);
     const data = await response.json();
     return data;
-  }
+  };
 
   const getRedditSourceRSS = async (source: Source) => {
     const url = `https://www.reddit.com/r/${source.name}.rss`;
     const response = await fetch(`/api/getRSSFeed?url=${url}`);
     const data = await response.json();
     return data;
-  }
+  };
 
   const getSummary = async () => {
     let prompt = "Here is a list of posts from sources the user cares about.";
@@ -59,7 +69,8 @@ export default function Home() {
       const source = sources[i];
       if (source && source.sourceType === "twitter") {
         const data = await getTwitterSourceRSS(source.name);
-        prompt += "Twitter user " + data.items[0].creator + "'s last ten tweets are:"
+        prompt +=
+          "Twitter user " + data.items[0].creator + "'s last ten tweets are:";
         for (let j = 0; j < 10; j++) {
           prompt += "\n" + j + ". " + data.items[j].title;
         }
@@ -71,105 +82,131 @@ export default function Home() {
         }
       }
     }
-    const response = await fetch(`/api/getSummary?prompt=${prompt}&openAIKey=${openAIKey}`);
+    const response = await fetch(
+      `/api/getSummary?prompt=${prompt}&openAIKey=${openAIKey}`,
+    );
     const data = await response.json();
     setSummary(data.summary);
-  }
+  };
 
   return (
     <>
       <Head>
         <title>Keep Up To Date</title>
-        <meta name="description" content="Keep up to date with your favorite twitter accounts and subreddits." />
+        <meta
+          name="description"
+          content="Keep up to date with your favorite twitter accounts and subreddits."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto p-6">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold mb-6">News Summarizer</h1>
-          <form className="w-full max-w-md" onSubmit={addSource}>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-source"
+      <div className="flex h-screen justify-center py-2">
+        <div className="flex w-min flex-col gap-2">
+          <div className="flex w-min gap-2">
+            <div className="flex flex-col gap-2">
+              <div className="flex aspect-square w-full items-center rounded-2xl bg-stone-700 text-center text-8xl font-bold text-white">
+                Quick News
+              </div>
+              <div className="flex w-min gap-2 rounded-[.5em] bg-neutral-200 p-2">
+                <input
+                  className="appearance-none rounded-[.5em] bg-neutral-100 p-2 text-center focus:outline-none"
+                  placeholder="Enter OpenAI Key"
+                  type="text"
+                  value={openAIKey}
+                  onChange={(e) => setOpenAIKey(e.target.value)}
+                />
+                <select
+                  onChange={(e) => setModelType(e.target.value)}
+                  className="appearance-none rounded-[.5em] bg-neutral-100 p-2 text-center focus:outline-none"
                 >
-                  News Source
-                </label>
-                <div className="relative inline-flex">
-                  <svg className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l189.21 189.211c9.372 9.373 24.749 9.373 34.121 0l189.211-189.211c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero" /></svg>
-                  <select onChange={(e) => setCurrentSource(e.target.value)} className="border border-gray-300 text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
-                    <option>Select Source Type</option>
-                    <option value="twitter">Twitter Account</option>
-                    <option value="reddit">Subreddit</option>
-                  </select>
-                </div>
-                {currentSource && (
-                  <>
-                    <input
-                      className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                      id="grid-source-input"
-                      placeholder={`Enter ${currentSource === 'twitter' ? 'Twitter handle' : 'Subreddit name'}`}
-                      type="text"
-                      value={newSource}
-                      onChange={(e) => setNewSource(e.target.value)}
-                    />
-                    <input
-                      className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                      id="grid-openai-key-input"
-                      placeholder="Enter OpenAI Key"
-                      type="text"
-                      value={openAIKey}
-                      onChange={(e) => setOpenAIKey(e.target.value)}
-                    />
-                    <Button className="mt-4" variant="outline" type="submit">
-                      Add Source
-                    </Button>
-                  </>
-                )}
+                  <option value="gpt-4">Select Model Type</option>
+                  <option value="gpt-4">GPT 4</option>
+                  <option value="gpt-3">GPT 3.5</option>
+                </select>
+              </div>
+              <Button
+                className="rounded-[.5em] bg-stone-700 text-white"
+                variant="outline"
+                onClick={() => getSummary()}
+              >
+                Summarize
+              </Button>
+            </div>
+            <div className="flex flex-col gap-2">
+              <form
+                className="flex w-min gap-2 rounded-[.5em] bg-neutral-200 p-2"
+                onSubmit={addSource}
+              >
+                <select
+                  onChange={(e) => setCurrentSource(e.target.value)}
+                  className="appearance-none rounded-[.5em] bg-neutral-100 p-2 text-center focus:outline-none"
+                >
+                  <option value="twitter">Select Source Type</option>
+                  <option value="twitter">Twitter</option>
+                  <option value="reddit">Reddit</option>
+                </select>
+                <input
+                  className="appearance-none rounded-[.5em] bg-neutral-100 text-center focus:outline-none"
+                  placeholder={`Enter ${
+                    currentSource === "twitter"
+                      ? "Twitter handle"
+                      : "Subreddit name"
+                  }`}
+                  type="text"
+                  value={newSource}
+                  onChange={(e) => setNewSource(e.target.value)}
+                />
+                <Button
+                  className="aspect-square rounded-[.5em] bg-stone-700 text-white"
+                  type="submit"
+                >
+                  ⏎
+                </Button>
+              </form>
+              <div className="h-full rounded-[.5em] bg-neutral-200 p-2">
+                {sources.map((source) => (
+                  <div
+                    className="rounded-[.5em] bg-neutral-100"
+                    key={source.name}
+                  >
+                    <div className="flex flex-row items-center justify-between p-3">
+                      <div className="flex flex-row items-center">
+                        <img
+                          alt={source.name}
+                          className="mr-2 rounded-full"
+                          height="40"
+                          src={source.image}
+                          style={{
+                            aspectRatio: "40/40",
+                            objectFit: "cover",
+                          }}
+                          width="40"
+                        />
+                        <div className="text-lg">{"@" + source.name}</div>
+                      </div>
+
+                      <Button
+                        size="icon"
+                        variant="ghost"
+                        onClick={() => deleteSource(source)}
+                      >
+                        <DeleteIcon className="aspect-square h-8 text-stone-700 hover:text-stone-400" />
+                      </Button>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-            <div className="w-full max-w-md mt-6">
-              <h2 className="text-2xl font-bold mb-4">Your Sources</h2>
-              {sources.map(source => (
-                <Card key={source.name}>
-                  <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-                    <img
-                      alt={source.name}
-                      className="rounded-full mr-4"
-                      height="40"
-                      src={source.image}
-                      style={{
-                        aspectRatio: "40/40",
-                        objectFit: "cover",
-                      }}
-                      width="40"
-                    />
-                    <CardTitle className="text-lg font-bold">{source.name}</CardTitle>
-                    <Button size="icon" variant="ghost" onClick={() => deleteSource(source)}>
-                      <DeleteIcon className="w-4 h-4 text-red-500" />
-                    </Button>
-                  </CardHeader>
-                </Card>
-              ))}
+          </div>
+          <div className="h-full overflow-y-scroll rounded-[.5em] bg-stone-200 p-4 ">
+            <div className="font-normal">
+              {summary ? summary : "Summary will appear here..."}
             </div>
-          </form>
-          <Button className="mt-10" variant="outline" onClick={() => getSummary()}>
-            Summarize
-          </Button>
-          <div className="w-full max-w-md mt-10">
-            <h2 className="text-2xl font-bold mb-4">Your Summary</h2>
-            <Card>
-              <CardContent>
-                <div className="text-base font-normal">{summary ? summary : "Summary will appear here..."}</div>
-              </CardContent>
-            </Card>
           </div>
         </div>
       </div>
     </>
   );
 }
-
 
 interface DeleteIconProps {
   className?: string;
@@ -193,5 +230,5 @@ function DeleteIcon({ className }: DeleteIconProps) {
       <line x1="18" x2="12" y1="9" y2="15" />
       <line x1="12" x2="18" y1="9" y2="15" />
     </svg>
-  )
+  );
 }
